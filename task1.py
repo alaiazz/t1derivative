02d1b89d4fc810a24cc33b13d23506cdbb2e83e7
Huayu <huayuz@umich.edu>
2024-04-22 16:07:00 -0500

    task 1.1, 1.2

--- task1.py
+++ task1.py
@@ -28,8 +28,11 @@ F7_26 =  get_data_from_csv(f'{data_root}/F7_26.csv')
 F7_27 =  get_data_from_csv(f'{data_root}/F7_27.csv')
 S_DQ_OPEN = get_data_from_csv(f'{data_root}/S_DQ_OPEN.csv')
 
-class test_info: 
-    def __init__(self, group_number, trading_frequency , initial_capital, trading_cost):
+# =============================================================================
+# # Backtesting Pipeline 
+# =============================================================================
+class TestInfo: 
+    def __init__(self, group_number = 10, trading_frequency = 'W', initial_capital = None, trading_cost =0.0):
         """
         Parameters:
         - group_number(int): Number of groups to divide into (default 10 for deciles).
@@ -43,12 +46,9 @@ class test_info:
         self.initial_capital = initial_capital 
         self.trading_cost = trading_cost 
 
-class one_factor_test: 
+class OneFactorTest: 
     def __init__(self,test_factor):
-        if test_factor == 'F7_26':
-            self.factor = F7_26
-        elif test_factor == 'F7_27':
-            self.factor == F7_27
+        self.factor = test_factor
         self.stock_price_open = S_DQ_OPEN
         self.restricted_stock_df = S_RESTRICT
         self.benchmark = S_905_DQ_RET
@@ -59,18 +59,176 @@ class one_factor_test:
         group_number = test_info.group_number - ((1 - percentile) * test_info.group_number) // 1
         return group_number
     
+    def trade_interval(self,df,test_info):
+        df_factor = df.resample(test_info.trading_frequency).first()   
+        df_factor = df_factor.ffill() 
+        return df_factor
+
     def trade(self,test_info): 
         df = self.rank_stock(test_info)
         df_price = self.stock_price_open
 
-        # initial_capital to trade
-        if test_info.initial_capital != 0: 
+        # decide initial capital to trade 
+        if test_info.initial_capital is not None: 
             initial_k = test_info.initial_capital
         else:
-            p_1_top = df.iloc[0] == 1
-            p_1_bottom = df.iloc[0] == 10
+            p_1_top = df.iloc[0] == 1.0
+            p_1_bottom = df.iloc[0] == 10.0
             p_1_top_capital = df_price.iloc[0][p_1_top].sum()
             p_1_bottom_capital = df_price.iloc[0][p_1_bottom].sum()
-            initial_k = 10 * (p_1_bottom_capital + p_1_top_capital)
-            side_k = initial_k / 2
+            initial_k = 1000 * (p_1_bottom_capital + p_1_top_capital)
+        side_k = initial_k / 2
+        top_pnl_tracker = {'total_pnl': [0], 'cumulative_pnl': [0]}
+        top_return_tracker = {'return': [0], 'net_return': [0]}
+        bottom_pnl_tracker = {'total_pnl': [0], 'cumulative_pnl': [0]}
+        bottom_return_tracker = {'net_return': [0]}
         
+        factor_resample = self.trade_interval(df,test_info)
+        price_resample = self.trade_interval(df_price, test_info)
+        top_r = factor_resample.copy() 
+        bottom_r = factor_resample.copy() 
+        
+        if (factor_resample.index != price_resample.index).any():
+            print('please debug')
+        for i, (date, signals) in enumerate(factor_resample.iterrows()):
+            # here equal weighted portfolio strategy is used, Markovitz, or CAPM optimization may give better result  
+            top = signals == 1.0
+            bottom = signals == 10.0
+            top_k = price_resample.loc[date][top].sum()
+            bottom_k = price_resample.loc[date][bottom].sum()
+            top_ratio = side_k / top_k if top_k else 0
+            bottom_ratio = side_k / bottom_k if bottom_k else 0
+            top_r.loc[date] = signals.apply(lambda x: top_ratio if x == 1 else 0)
+            bottom_r.loc[date] = signals.apply(lambda x: bottom_ratio if x == 1 else 0)
+
+            # profit calculation 
+            if i > 0: 
+                price_change = price_resample.iloc[i] - price_resample.iloc[i-1]
+                
+                top_pnl = (top_r.iloc[i-1] * price_change).sum()
+                bottom_pnl = (bottom_r.iloc[i-1] * price_change).sum()
+        
+                top_cumulative_pnl = top_pnl_tracker['cumulative_pnl'][i-1] + top_pnl
+                bottom_cumulative_pnl = bottom_pnl_tracker['cumulative_pnl'][i-1] + bottom_pnl
+
+                top_pnl_tracker['total_pnl'].append(top_pnl)
+                top_pnl_tracker['cumulative_pnl'].append(top_cumulative_pnl)
+
+                bottom_pnl_tracker['total_pnl'].append(bottom_pnl)
+                bottom_pnl_tracker['cumulative_pnl'].append(bottom_cumulative_pnl)
+
+                top_return_tracker['net_return'].append(top_pnl/side_k - test_info.trading_cost)
+                bottom_return_tracker['net_return'].append(bottom_pnl/side_k - test_info.trading_cost)
+        df_top_pnl_tracker = pd.DataFrame(top_pnl_tracker, index=factor_resample.index)
+        df_top_return_tracker = pd.DataFrame(top_return_tracker, index=factor_resample.index)
+        df_top_performance = pd.concat([df_top_pnl_tracker, df_top_return_tracker], axis=1)
+        df_top_performance['cumulative_return'] = ((df_top_performance['net_return'] + 1).cumprod() -1)/100
+
+        df_bottom_pnl_tracker = pd.DataFrame(bottom_pnl_tracker, index=factor_resample.index)
+        df_bottom_return_tracker = pd.DataFrame(bottom_return_tracker, index=factor_resample.index)
+        df_bottom_performance = pd.concat([df_bottom_pnl_tracker, df_bottom_return_tracker], axis=1)
+        df_bottom_performance['cumulative_return'] = ((df_bottom_performance['net_return'] + 1).cumprod() -1)/100
+
+        return df_top_performance, df_bottom_performance
+    
+    def evaluation_mectrics(self,name,performance,adj1 = 48,var=0.05): 
+        summary = dict()
+        data = performance.loc[:,['net_return']].dropna()
+        summary["Annualized Return"] = data.mean() * adj1
+        summary["Annualized Volatility"] = data.std() * np.sqrt(adj1)
+        summary["Annualized Sharpe Ratio"] = (
+            summary["Annualized Return"] / summary["Annualized Volatility"]
+        )
+        summary["Annualized Sortino Ratio"] = summary["Annualized Return"] / (
+            data[data < 0].std() * np.sqrt(adj1)
+        )
+
+        summary["Skewness"] = data.skew()
+        summary["Excess Kurtosis"] = data.kurtosis()
+        summary[f"VaR ({var})"] = data.quantile(var, axis=0)
+        summary[f"CVaR ({var})"] = data[data <= data.quantile(var, axis=0)].mean()
+        summary["Min"] = data.min()
+        summary["Max"] = data.max()
+
+        wealth_index = 1000 * (1 + data).cumprod()
+        previous_peaks = wealth_index.cummax()
+        drawdowns = (wealth_index - previous_peaks) / previous_peaks
+
+        summary["Max Drawdown"] = drawdowns.min()
+
+        summary["Bottom"] = drawdowns.idxmin()
+        summary["Peak"] = previous_peaks.idxmax()
+        return pd.DataFrame(summary) 
+    
+    def eval_combined(self,test_info): 
+        per_top, per_bottom = self.trade(test_info)
+        top = self.evaluation_mectrics('top',per_top,adj1 = 48,var=0.05)
+        bottom = self.evaluation_mectrics('bottom',per_bottom,adj1 = 48,var=0.05)
+        return pd.concat([top,bottom], axis=0,keys=['top', 'bottom'])
+    
+    def compare_with_benchmark(self,test_info):
+        per_top, per_bottom = self.trade(test_info)
+        ret_top = per_top.loc[:,['net_return']].dropna()
+        ret_bottom = per_bottom.loc[:,['net_return']].dropna()
+ 
+
+        daily_resampled_top = ret_top.resample('D').ffill().div(5)
+        daily_resampled_bottom = ret_bottom.resample('D').ffill().div(5)
+        daily_resampled_top = daily_resampled_top.reindex(self.benchmark.index, method='ffill')  
+        daily_resampled_bottom = daily_resampled_bottom.reindex(self.benchmark.index, method='ffill') 
+
+        daily_resampled_bottom.fillna(0, inplace= True)
+        daily_resampled_top.fillna(0, inplace = True)
+
+        if (self.benchmark.index != daily_resampled_bottom.index).any(): 
+            print('please debug')
+        summary = pd.DataFrame(index = self.benchmark.index) 
+        summary["Daily excess_return TOP"] = (daily_resampled_top['net_return'] - self.benchmark[905])
+        summary["Cumulative excess_return TOP"] = (1 + summary['Daily excess_return TOP']).cumprod() - 1
+        summary["Daily excess_return BOTTOM"] = (daily_resampled_top['net_return'] - self.benchmark[905])
+        summary["Cumulative excess_return BOTTOM"] = (1 + summary['Daily excess_return BOTTOM']).cumprod() - 1
+
+        return summary
+
+
+# =============================================================================
+# # Factor Coverage Rate 
+# =============================================================================
+
+def calc_factor_coverage_rate(df_restrict, df_factor, factor_name):
+    # check df info 
+    if (df_restrict.index != df_factor.index).any(): 
+        print('Please debug: length does not match.')
+    elif (df_restrict.columns != df_factor.columns).any(): 
+        print('Please debug: columns do not match.')
+    df_factor_shifted = df_factor.shift(1).iloc[1:]
+    df_restrict_aligned = df_restrict.iloc[:-1]
+    df_restrict_aligned.index = df_factor_shifted.index
+    stock_able_to_trade = (df_restrict_aligned == 0).sum(axis=1)
+    factor_cover = ((df_restrict_aligned == 0) & (~df_factor_shifted.isna())).sum(axis=1)
+    coverage_rate = factor_cover/stock_able_to_trade
+    coverage_rate = coverage_rate.fillna(0)
+    return coverage_rate.to_frame(name=factor_name) 
+
+def factor_coverage_rate_plot(df_restrict = S_RESTRICT, df_factor1 = F7_26, df_factor2 = F7_27, factor_name1= 'F7_26', factor_name2 = 'F7_27'):
+    coverage_26 = calc_factor_coverage_rate(df_restrict,df_factor1,factor_name1)
+    coverage_27 = calc_factor_coverage_rate(df_restrict,df_factor2,factor_name2)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(coverage_26.index, coverage_26['F7_26'], label='F7_26 Factor Coverage Rate')
+    plt.plot(coverage_27.index, coverage_27['F7_27'], label='F7_27 Factor Coverage Rate')
+
+    plt.title('Comparison of Coverage Rate Over Time')
+    plt.xlabel('Date')
+    plt.ylabel('Factor Coverage Rate')
+    plt.legend()
+
+    plt.show()
+
+
+
+
+
+# =============================================================================
+# # 因子市值中性化 
+# =============================================================================
